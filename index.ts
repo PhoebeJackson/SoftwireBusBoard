5be04c809fce001da423769d2ae129f915c77397
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2021-07-01 13:58:24 +0100

    Completed part 1
    Sorted incoming busses, sliced the array and printed the results

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-// prompt, request, read, sort, print
-// 490008660N
 const myPrompt = require('prompt-sync')();
 import axios from 'axios';
 import {DateTime} from "luxon";
@@ -14,20 +12,29 @@ class incomingBus{
         this.destination = row.destinationName;
         this.arrivalTime = DateTime.fromISO(row.expectedArrival)
     }
+
+    toString() {
+        let minutes = this.arrivalTime.diff(DateTime.now(), 'minutes').minutes
+        minutes = Math.round(minutes)
+        return `\nRoute ${this.route} bus heading to ${this.destination} is expected in: ${minutes} mins`
+    }
 }
 
-// const stopID = myPrompt('Stop ID: ');
-const stopID = '490008660N'
+const stopID = myPrompt('Stop ID: ');
+// const stopID = '490008660N'
 const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`
 const incomingBuses: incomingBus[] = [];
-console.log(requestURL)
 axios.get(requestURL)
     .then((response) => {
-        console.log(response.data);
+        // console.log(response.data);
         response.data.forEach((element: any) => {
             let bus = new incomingBus(element);
             incomingBuses.push(bus);
         })
-        console.log(incomingBuses);
+
+        incomingBuses.sort(function(lhs, rhs) {
+            return lhs.arrivalTime.toMillis() - rhs.arrivalTime.toMillis()
+        })
+        console.log(incomingBuses.slice(0, 5).toString())
     });
 
