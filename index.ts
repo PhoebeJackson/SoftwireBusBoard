8ac5bbeebb81cbbfc353ab6956c606259af998fe
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2021-07-01 11:58:43 +0100

    Installed Axios, created URL

---
+++ index.ts
@@ -0,0 +1,9 @@
+// prompt, request, read, sort, print
+// 490008660N
+const myPrompt = require('prompt-sync')();
+import axios from 'axios'
+
+// const stopID = myPrompt('Stop ID: ');
+const stopID = '490008660N'
+const requestURL = `https://api.tfl.gov.uk/StopPoint/${stopID}/Arrivals`
+console.log(requestURL)
