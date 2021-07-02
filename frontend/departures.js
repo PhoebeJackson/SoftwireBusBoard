f59c8c466d64550127e72ea827d12d279d9a1b5f
Ricky Kawagishi <ricky.kawagishi@softwire.com>
2021-07-02 15:28:58 +0100

    Show results

--- frontend/departures.js
+++ frontend/departures.js
@@ -6,7 +6,36 @@ function onEnterPostcode() {
     xhttp.setRequestHeader('Content-Type', 'application/json');
     xhttp.onload = function() {
         // Handle response here using e.g. xhttp.status, xhttp.response, xhttp.responseText
-        console.log(xhttp.response)
+        showResults(xhttp.response)
     }
     xhttp.send();
+}
+
+function showResults(response) {
+    var jsonObj = JSON.parse(response)
+    var stopNames = []
+    for (var stop in jsonObj) {
+        stopNames.push(stop)
+    }
+    document.getElementById('stop1').innerHTML = stopNames[0]
+    document.getElementById('stop2').innerHTML = stopNames[1]
+    var list1 = document.getElementById('list1')
+    var buses1 = jsonObj[stopNames[0]]
+    buses1.forEach(bus => {
+        var listElem = document.createElement('li')
+        listElem.innerHTML = bus2string(bus)
+        list1.appendChild(listElem)
+    })
+
+    var list2 = document.getElementById('list2')
+    var buses2 = jsonObj[stopNames[1]]
+    buses2.forEach(bus => {
+        var listElem = document.createElement('li')
+        listElem.innerHTML = bus2string(bus)
+        list2.appendChild(listElem)
+    })
+}
+
+function bus2string(bus) {
+    return `Route ${bus.route} bus heading to ${bus.destination} is expected in: ${bus.minutesToArrival} mins`
 }
